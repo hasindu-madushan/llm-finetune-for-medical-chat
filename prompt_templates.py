e0451a352f94fcefeac2cb2ef4d8e3eba4489793
hasindu <hasindumadushan325@gmail.com>
2025-05-16 10:24:53 +0000

    Create domain bound qna data prep and finetuen scripts

---
+++ prompt_templates.py
@@ -0,0 +1,9 @@
+domain_bound_qna_prompt_template = """
+# Instruction:
+Assume you are an excellent doctor. Using your knowledge, answer the question given below. You should only asnwer questions related medical and healthcare domain.
+
+# Question: {question}
+
+# Answer: """
+
+domain_bound_qna_prompt_template = prompt_template.strip()
